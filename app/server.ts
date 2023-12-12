f5e5c380f0af058be5242030bf1b46660bbaa380
wataru <woguchi@routeware.com>
2023-12-11 19:53:03 -0800

    Provide the path that delivers assets

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,16 @@
+import { serveStatic as honoServeStatic } from "@hono/node-server/serve-static";
 import { createApp } from "@sonikjs/react";
 import { serveStatic } from "hono/cloudflare-pages";
 
 const app = createApp();
 app.use("/static/*", serveStatic());
+// The `serveStatic` exposed by "hono/cloudflare-pages" does not take a parameter.
+app.use(
+  "/assets/*",
+  honoServeStatic({
+    root: "./",
+    rewriteRequestPath: (path) => path.replace(/^\/assets/, "/assets"),
+  }),
+);
 
 export default app;
